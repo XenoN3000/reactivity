b7cda9dfdd64599617331cb669c4bc3d3bbbd948
XenoN3000 <hosein.nahari3000@gmail.com>
2025-01-21 19:42:14 +0330

    liara-21

--- API/Extensions/ServiceExtensions.cs
+++ API/Extensions/ServiceExtensions.cs
@@ -111,17 +111,24 @@ public static class ServiceExtensions
                 var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                 // Parse connection URL to connection string for Npgsql
-                connUrl = connUrl.Replace("postgres://", string.Empty);
-                var pgUserPass = connUrl.Split("@")[0];
-                var pgHostPortDb = connUrl.Split("@")[1];
-                var pgHostPort = pgHostPortDb.Split("/")[0];
-                var pgDb = pgHostPortDb.Split("/")[1];
-                var pgUser = pgUserPass.Split(":")[0];
-                var pgPass = pgUserPass.Split(":")[1];
-                var pgHost = pgHostPort.Split(":")[0];
-                var pgPort = pgHostPort.Split(":")[1];
+                // connUrl = connUrl.Replace("postgres://", string.Empty);
+                // var pgUserPass = connUrl.Split("@")[0];
+                // var pgHostPortDb = connUrl.Split("@")[1];
+                // var pgHostPort = pgHostPortDb.Split("/")[0];
+                // var pgDb = pgHostPortDb.Split("/")[1];
+                // var pgUser = pgUserPass.Split(":")[0];
+                // var pgPass = pgUserPass.Split(":")[1];
+                // var pgHost = pgHostPort.Split(":")[0];
+                // var pgPort = pgHostPort.Split(":")[1];
 
-                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
+                var pgHost = "reactivities";
+                var pgPort = "5432";
+                var pgUser = "root";
+                var pgPass = "y2ZBlbRa3WA3cQ4GK90vcGqV";
+                var pgDb = "reactivities";
+                
+
+                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; Trust Server Certificate=true";
             }
             opt.UseNpgsql(connStr);
         });
