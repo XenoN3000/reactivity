9e7d6b7aed73b6958cbc7fa258c1d3401bea6f79
XenoN3000 <hosein.nahari3000@gmail.com>
2025-01-22 12:02:06 +0330

    liara-36

--- API/Program.cs
+++ API/Program.cs
@@ -21,11 +21,15 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
